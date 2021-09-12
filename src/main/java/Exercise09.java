51bb234c50be0b3043979a397cd14de91b40f241
magdasobrino <magdalena.sobrino@knights.ucf.edu>
2021-09-12 15:18:37 -0400

    Initial commit

---
+++ src/main/java/Exercise09.java
@@ -0,0 +1,37 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Magdalena Sobrino-Almanzar
+ */
+package exercise;
+import java.util.Scanner;
+import java.lang.*;
+
+public class Exercise09 {
+
+        public static void main(String[] args){
+
+            double conversionFactor = 350;
+            int length = 0, width = 0;
+            int squareFeet;
+            double gallons;
+            Scanner scnr = new Scanner(System.in);
+
+            System.out.println("What is the length of the room in feet?");
+            length = scnr.nextInt();
+            System.out.println("What is the width of the room in feet?");
+            width = scnr.nextInt();
+
+            squareFeet = length * width;
+
+            gallons = squareFeet / conversionFactor;
+
+            System.out.println("You will need to purchase " + (int) Math.ceil(gallons) + " gallons of paint to cover " + squareFeet + " square feet.");
+
+            scnr.close();
+            System.exit(0);
+        }//end of main
+    }//end of program
+
+
+
+
